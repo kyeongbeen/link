79f8525cf6359fcf0a6af69529325e5088f335d2
KyeongBin <kyeongbin0205@naver.com>
2025-01-08 17:21:48 +0900

    chore: TaskDetail.js 수정
    살펴볼게 있어 바꿨던것 원 상태로 복구했음

--- link/src/components/Task/TaskDetail.js
+++ link/src/components/Task/TaskDetail.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DialogContent from "@mui/material/DialogContent";
-import { Typography, Box, Grid2, Divider } from "@mui/material";
+import { Typography, Box, Grid, Divider } from "@mui/material";
 
 const TaskDetail = ({ task }) => {
   // task가 null인 경우 렌더링하지 않음
@@ -11,65 +11,65 @@ const TaskDetail = ({ task }) => {
   return (
     <DialogContent>
       <Box>
-        <Grid2 container spacing={2}>
+        <Grid container spacing={2}>
           {/* 작업 제목 */}
-          <Grid2 item xs={12}>
+          <Grid item xs={12}>
             <Typography variant="h6" color="primary">
               작업 제목
             </Typography>
             <Typography variant="body1">{task.title}</Typography>
-          </Grid2>
+          </Grid>
 
           <Divider sx={{ my: 2, width: "100%" }} />
 
           {/* 우선순위 */}
-          <Grid2 item xs={6}>
+          <Grid item xs={6}>
             <Typography variant="subtitle1" fontWeight="bold">
               우선순위
             </Typography>
             <Typography variant="body1">{task.priority}</Typography>
-          </Grid2>
+          </Grid>
 
           {/* 상태 */}
-          <Grid2 item xs={6}>
+          <Grid item xs={6}>
             <Typography variant="subtitle1" fontWeight="bold">
               상태
             </Typography>
             <Typography variant="body1">{task.status}</Typography>
-          </Grid2>
+          </Grid>
 
           <Divider sx={{ my: 2, width: "100%" }} />
 
           {/* 진행기간 */}
-          <Grid2 item xs={12}>
+          <Grid item xs={12}>
             <Typography variant="subtitle1" fontWeight="bold">
               진행기간
             </Typography>
             <Typography variant="body1">
               {task.startDate} ~ {task.endDate}
             </Typography>
-          </Grid2>
+          </Grid>
 
           <Divider sx={{ my: 2, width: "100%" }} />
 
           {/* 담당자 */}
-          <Grid2 item xs={6}>
+          <Grid item xs={6}>
             <Typography variant="subtitle1" fontWeight="bold">
               담당자
             </Typography>
             <Typography variant="body1">{task.author}</Typography>
-          </Grid2>
+          </Grid>
 
           <Divider sx={{ my: 2, width: "100%" }} />
 
           {/* 내용 */}
-          <Grid2 item xs={12}>
+          <Grid item xs={12}>
             <Typography variant="subtitle1" fontWeight="bold">
               내용
             </Typography>
             <Typography variant="body1">{task.content}</Typography>
-          </Grid2>
-        </Grid2>
+          </Grid>
+        </Grid>
       </Box>
     </DialogContent>
   );
