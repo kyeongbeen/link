c2f357bb5db4ad7740be0483731a55068c071739
luvnuyh <sohyun5429@gmail.com>
2025-01-10 15:22:47 +0900

    build

--- link/src/setupTests.js
+++ link/src/setupTests.js
@@ -2,4 +2,17 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+//import '@testing-library/jest-dom';
+
+/** build를 위해 아래 내용 추가
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+        }));
+};
+*/
