8ff2397409218eb6d07e0096b8678599e1f2df30
KimSanghun <122427631+sanghunkim20@users.noreply.github.com>
2025-01-10 14:23:58 +0900

    feat: 게시판 API 모든 메소드 구현
    Swagger에 나와 있는 명세서대로 모든 메소드 구현, 테스트 완료헀습니다.
    
    현재 로그인, 회원가입을 구현하지 않아 작성자, 프로젝트ID는 임시로 더미 데이터 넣어서 테스트 진행했어요.

--- link/src/components/Board/Board.js
+++ link/src/components/Board/Board.js
@@ -17,20 +17,18 @@ import { DialogContent, DialogContentText } from "@mui/material";
 import dayjs from "dayjs";
 import axios from "axios";
 
-/**
- * 추후 API 연동할 때 useState에 author 추가
- */
 const Board = () => {
   const postPerPage = 5; // 페이지당 게시글 수
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
-  const [selectedPost, setSelectedPost] = useState(null); // 선택된 게시글
+  const [selectedPost, setSelectedPost] = useState({}); // 선택된 게시글
   const [openDialog, setOpenDialog] = useState(false); // 다이얼로그 열림 여부
   const [isEditing, setIsEditing] = useState(false); // 수정 중인지 여부
   const [editedPost, setEditedPost] = useState(null); // 수정 중인 게시글
-  const [newReply, setNewReply] = useState(""); // 새로운 댓글 내용
+  const [replys, setReplys] = useState([]); // 댓글 목록
   const [openCreateDialog, setOpenCreateDialog] = useState(false); // 글 작성 다이얼로그 열림 여부
   const [posts, setPosts] = useState([]); // 게시글 목록
   const [newPost, setNewPost] = useState(0); // 새로운 게시글 내용
+  const [newReply, setNewReply] = useState(0); // 새로운 댓글 내용
 
   // 날짜 변환 함수
   const formatDateToKrTime = (date) => {
@@ -38,28 +36,28 @@ const Board = () => {
       year: "numeric",
       month: "2-digit",
       day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
     };
-    const dayjsDate = dayjs(date); // dayjs로 변환
-    return dayjsDate.format("YYYY-MM-DD"); // 원하는 형식으로 변환
+    const dayjsDate = dayjs(date);
+    return dayjsDate.format("YYYY-MM-DD HH:mm");
   };
 
   // 게시글 목록 불러오기
   useEffect(() => {
     const getPosts = async () => {
       try {
-        // 게시글 목록 API 호출
         const response = await axios.get("http://localhost:8080/post/list");
-        setPosts(response.data); // API 데이터로 상태 업데이트
+        setPosts(response.data);
 
         setPosts((prevPosts) =>
           prevPosts.map((post) => ({
             ...post,
-            date: formatDateToKrTime(post.createDate),
+            date: formatDateToKrTime(post.createdDate),
           }))
         );
-
-      } catch(error) {
-        console.error("게시글 목록을 가져오는 중 오류 발생: ",error);
+      } catch (error) {
+        console.error("게시글 목록을 가져오는 중 오류 발생: ", error);
       }
     };
 
@@ -93,126 +91,145 @@ const Board = () => {
 
   // 게시글 수정
   const handleEdit = () => {
-    setIsEditing(true); // 수정 모드로 변경
+    setIsEditing(true);
   };
 
-  const handleSaveEdit = () => {
-    // 수정할 게시글에서 기존 댓글을 포함
-    const updatedPost = {
-      ...editedPost,
-      replys: selectedPost.replys, // 기존 댓글 유지
-    };
-
-    setPosts((prevPosts) =>
-      prevPosts.map((post) =>
-        post.id === updatedPost.id ? { ...post, ...updatedPost } : post
-      )
-    );
-
-    setIsEditing(false); // 수정 모드 해제
-    setSelectedPost(updatedPost); // 수정된 게시글로 선택된 게시글 변경
-  };
-
-  // 게시글 삭제
-  const handleDelete = () => {
-    setPosts((prevPosts) =>
-      prevPosts.filter((post) => post.id !== selectedPost.id)
-    );
-    handleCloseDialog();
-  };
-
-  // 댓글 추가
-  const handleAddReply = () => {
-    if (newReply.trim()) {
-      setPosts((prevPosts) => {
-        const updatedPosts = prevPosts.map((post) =>
-          post.id === selectedPost.id
-            ? {
-                ...post,
-                replys: [
-                  ...post.replys,
-                  {
-                    author: "댓글 작성자",
-                    content: newReply,
-                    date: formatDateToKrTime(new Date()),
-                  },
-                ],
-              }
-            : post
-        );
-        const updatedPost = updatedPosts.find(
-          (post) => post.id === selectedPost.id
-        );
-        setSelectedPost(updatedPost);
-        return updatedPosts;
-      });
-      setNewReply(""); // 댓글 입력 필드 초기화
+  // 수정 저장
+  const handleSaveEdit = async () => {
+    try {
+      const response = await axios.post(
+        `http://localhost:8080/post/update/${selectedPost.postId}?title=${encodeURIComponent(editedPost.title)}&content=${encodeURIComponent(editedPost.content)}`
+      );
+      editedPost.date = formatDateToKrTime(editedPost.createdDate);
+      setPosts((prevPosts) =>
+        prevPosts.map((p) => (p.postId === editedPost.postId ? editedPost : p))
+      );
+      setIsEditing(false);
+      alert("게시글이 수정되었습니다.");
+      handleCloseDialog();
+    } catch (error) {
+      console.error("게시글을 수정하는 중 오류 발생: ", error);
     }
   };
 
+  // 게시글 삭제
+  const handleDelete = async (post, reply) => {
+    setSelectedPost(post);
+    if (reply) {
+      const replyURL = `http://localhost:8080/reply/delete/${reply.replyId}`;
+      await axios.post(replyURL);
+    }
+    const boardURL = `http://localhost:8080/post/delete/${post.postId}`;
+    try {
+      await axios.get(boardURL);
+      setPosts((prevPosts) =>
+        prevPosts.filter((post) => post.postId !== selectedPost.postId)
+      );
+      handleCloseDialog();
+      alert("게시글이 삭제되었습니다.");
+    } catch (error) {
+      console.error("게시글을 삭제하는 중 오류 발생: ", error);
+      return null;
+    }
+  };
+
+  // 댓글 추가
+  const handleAddReply = async () => {
+    const newReplyData = {
+      projectId: 0, // 임시
+      authorId: 0, // 임시
+      content: newReply.content,
+      createdDate: newReply.createDate,
+      postId: selectedPost.postId,
+    };
+    console.log("댓글 추가 데이터:", newReplyData);
+    try {
+      const response = await axios.post(
+        `http://localhost:8080/reply/create/${selectedPost.postId}`,
+        newReplyData
+      );
+      const newReply = response.data;
+      setReplys((prevReplies) => [...prevReplies, newReply]);
+      await getReply();
+      setNewReply({
+        content: "",
+        createDate: new Date(),
+      });
+    } catch (error) {
+      console.error("댓글 생성 중 오류 발생:", error);
+    }
+  };
+
+  // 댓글 목록 불러오기 함수
+  const getReply = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/reply/list");
+      setReplys(response.data);
+    } catch (error) {
+      console.error("댓글 목록을 가져오는 중 오류 발생: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getReply();
+  }, []);
+
   // 댓글 삭제
-  const handleDeleteReply = (reply) => {
-    console.log(reply);
-    setPosts((prevPosts) => {
-      const updatedPosts = prevPosts.map((post) =>
-        post.id === selectedPost.id
-          ? {
-              ...post,
-              replys: post.replys.filter(
-                (currentReply) => currentReply !== reply
-              ),
-            }
-          : post
+  const handleDeleteReply = async (reply) => {
+    const URL = `http://localhost:8080/reply/delete/${reply.replyId}`;
+    try {
+      const response = await axios.post(URL);
+      setReplys((prevReplies) =>
+        prevReplies.filter(
+          (existingReply) => existingReply.replyId !== reply.replyId
+        )
       );
-      const updatedPost = updatedPosts.find(
-        (post) => post.id === selectedPost.id
-      );
-      setSelectedPost(updatedPost);
-      return updatedPosts;
-    });
+      alert("댓글이 삭제되었습니다.");
+    } catch (error) {
+      console.error("댓글을 삭제하는 중 오류 발생: ", error);
+      return null;
+    }
   };
 
   // 새로운 게시글 추가
   const handleCreatePost = async () => {
     const newPostData = {
-      authorId: 1, // 임시 (테스트를 위함)
-      title: newPost.title,
-      content: newPost.content,
-      createDate: formatDateToKrTime(new Date()),
-      projectId: 1, // 임시 (테스트를 위함)
+      ...newPost,
     };
-    console.log("POST로 보내질 데이터: ", newPostData); // null 값이 있나 확인을 위함
+    console.log("POST로 보내질 데이터: ", newPostData);
     try {
-      // 게시글 생성 API 호출
-      const response = await axios.post("http://localhost:8080/post/create", 
-        newPostData);
-      console.log("새로 생성된 게시글: ", response.data);
-      
+      const response = await axios.post(
+        "http://localhost:8080/post/create",
+        newPostData
+      );
       // 새로 데이터 가져오기 (새로 생성된 게시글이 반영이 안 돼서 그냥 다시 불러옴)
       const getResponse = await axios.get("http://localhost:8080/post/list");
       setPosts(getResponse.data);
+
       setPosts((prevPosts) =>
         prevPosts.map((post) => ({
           ...post,
-          date: formatDateToKrTime(post.createDate),
+          date: formatDateToKrTime(post.createdDate),
         }))
-      )
-
-      // 다이얼로그 닫기
+      );
       setOpenCreateDialog(false);
+      setNewPost({
+        title: "",
+        content: "",
+      });
 
       alert("새 게시글이 작성되었습니다.");
     } catch (error) {
       console.error("게시글을 추가하는 중 오류 발생: ", error);
     }
-  }
+  };
 
   return (
     <div style={{ padding: "20px" }}>
       <Button
         variant="contained"
         color="primary"
-        onClick={() => setOpenCreateDialog(true)} // 글 작성 다이얼로그 열기
+        onClick={() => setOpenCreateDialog(true)}
         style={{ marginBottom: "20px" }}
       >
         글 작성하기
@@ -287,39 +304,53 @@ const Board = () => {
             ) : (
               <>
                 <Button onClick={handleEdit}>수정</Button>
-                <Button onClick={handleDelete} color="error">
+                <Button
+                  onClick={() =>
+                    handleDelete(
+                      selectedPost,
+                      replys.find(
+                        (reply) => reply.postId === selectedPost?.postId // 댓글도 삭제되기 위하여 reply도 매개변수로 줌
+                      )
+                    )
+                  }
+                  color="error"
+                >
                   삭제
                 </Button>
               </>
             )}
             <Button onClick={handleCloseDialog}>닫기</Button>
           </DialogActions>
-
           {/* 댓글 영역 */}
           {!isEditing &&
-            selectedPost?.replys?.map((reply, index) => (
-              <div
-                key={index}
-                style={{
-                  borderBottom: "1px solid #ddd",
-                  paddingBottom: "10px",
-                  marginBottom: "10px",
-                }}
-              >
-                <p>
-                  <strong>{reply.author}</strong> ({reply.date})
-                </p>
-                <p>{reply.content}</p>
-                <DialogActions>
-                  <Button
-                    onClick={() => handleDeleteReply(reply)}
-                    color="error"
-                  >
-                    삭제
-                  </Button>
-                </DialogActions>
-              </div>
-            ))}
+            replys
+              .filter((reply) => reply.postId === selectedPost?.postId)
+              .map((reply, index) => (
+                <div
+                  key={index}
+                  style={{
+                    borderBottom: "1px solid #ddd",
+                    paddingBottom: "10px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  <p>
+                    {" "}
+                    {/* 현재 로그인 유저는 가져 올 수 없기 때문에 '미구현'으로 출력 */}
+                    <strong>미구현</strong> (
+                    {formatDateToKrTime(reply.createdDate)})
+                  </p>
+                  <p>{reply.content}</p>
+                  <DialogActions>
+                    <Button
+                      onClick={() => handleDeleteReply(reply)}
+                      color="error"
+                    >
+                      삭제
+                    </Button>
+                  </DialogActions>
+                </div>
+              ))}
 
           {/* 댓글 입력 필드 */}
           {!isEditing && (
@@ -327,8 +358,10 @@ const Board = () => {
               <TextField
                 fullWidth
                 label="댓글을 입력하세요."
-                value={newReply}
-                onChange={(e) => setNewReply(e.target.value)}
+                value={newReply.content}
+                onChange={(e) =>
+                  setNewReply({ ...newReply, content: e.target.value })
+                }
                 multiline
                 rows={1}
               />
