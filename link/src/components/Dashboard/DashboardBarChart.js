77a55097d06ac60041997c3bcfb8a3fc7a4f42f6
onlyinhere3113 <onlyinhere3113@gmail.com>
2025-01-12 18:04:41 +0900

    차트 생성

---
+++ link/src/components/Dashboard/DashboardBarChart.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+// Chart.js 필수 설정 등록
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const DashboardBarChart = ({ data }) => {
+  // 차트 데이터 정의
+  const chartData = {
+    labels: ["TOTAL", "FINISH", "ONGOING", "INCOMPLETE"],
+    datasets: [
+      {
+        label: "Tasks",
+        data: [
+          data.totalTasks,
+          data.finishedTasks,
+          data.ongoingTasks,
+          data.inCompletedTasks,
+        ],
+        backgroundColor: [
+          "#1976D2", // Primary
+          "#388E3C", // Success
+          "#FBC02D", // Warning
+          "#D32F2F", // Error
+        ],
+      },
+    ],
+  };
+
+  // 차트 옵션 정의
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false, // 차트의 비율을 부모 요소에 맞춤
+    plugins: {
+      legend: {
+        display: true, // 범례 표시 여부
+        position: "top", // 범례 위치
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true, // Y축이 0에서 시작
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={chartOptions} />;
+};
+
+export default DashboardBarChart;
